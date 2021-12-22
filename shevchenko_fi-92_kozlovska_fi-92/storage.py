46028b29d4f76c2ebe9d75c5e409b968f1f4f66c
Yuliashaaa <ulka.shevchenko@gmail.com>
2021-12-22 20:50:56 +0200

    Update storage.py

--- shevchenko_fi-92_kozlovska_fi-92/storage.py
+++ shevchenko_fi-92_kozlovska_fi-92/storage.py
@@ -19,17 +19,34 @@ class db:
             if index_flag == True:
                 self.index[i] = indexColumns()
 
+    def insert(self, values):
+        rows_insert = []
+        if len(values) == len(self.columns):
+            
+
 # Storager
 class storager:
     def __init__(self):
         self.table = {}
 
+    # Create Database
     def create_db(self, table_name, columns):
         if columns:
             self.table[table_name] = db(table_name, columns)
             print(f"Table {table_name} has been successfully created!")
         else:
             print(f"Table {table_name} columns are empty!")
+
+    # Insert data to database
+    def insert_db(self, table_name, values):
+        if table_name not in self.table:
+            print(f"Table {table_name} does not exist!")
+        else:
+            if len(values) != len(self.table[table_name].columns):
+                print(f"Invalid amount of values to insert into {table_name}!")
+            else:
+                rows_insert = self.table[table_name].insert(values)
+                print(f"{len(rows_insert)} row(s) has been inserted into {table_name}!")
 
 if __name__ == "__main__":
     storage = storager()
