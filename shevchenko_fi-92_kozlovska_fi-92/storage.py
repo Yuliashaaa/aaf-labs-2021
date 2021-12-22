723342dab3e1f17303a29eb581e2bdd766dff772
Yuliashaaa <ulka.shevchenko@gmail.com>
2021-12-22 23:56:27 +0200

    Update storage.py

--- shevchenko_fi-92_kozlovska_fi-92/storage.py
+++ shevchenko_fi-92_kozlovska_fi-92/storage.py
@@ -5,24 +5,42 @@ class indexColumns:
     def __init__(self):
         self.container = SortedDict()
 
+    def insert(self, value, pointer):
+        # Return key value if it is available in the dictionary
+        self.container.setdefault(value, set()).add(pointer)
+
 # Database 
 class db:
     def __init__(self, table_name, columns):
         self.name = table_name
         self.table = {}
-        self.row_id = 0
         self.columns = {}
+        self.row_id = 0
         self.index = {}
 
+        # Indexing
         for i, (column_name, index_flag) in enumerate(columns):
             self.columns[column_name] = i
             if index_flag == True:
                 self.index[i] = indexColumns()
 
-    def insert(self, values):
+    # Insert rows to database table
+    def insert(self, table_name, values):
         rows_insert = []
-        if len(values) == len(self.columns):
-            
+        if len(values) != len(self.columns):
+            print(f"Invalid amount of values to insert into {table_name}!")
+        else:
+            print(f"len val {len(values)}   len cols {len(self.columns)}")
+            row = self.row_id
+            print(f"row  {row}")
+            for column_name in self.index:
+                print(f"cols   {column_name}")
+                row_i = self.columns[column_name]
+                self.index[column_name].insert(values[row_i], row)
+            self.table[row] = values
+            self.row_id += 1
+            rows_insert.append(values)
+        return rows_insert
 
 # Storager
 class storager:
@@ -37,7 +55,7 @@ class storager:
         else:
             print(f"Table {table_name} columns are empty!")
 
-    # Insert data to database
+    # Insert data to storage
     def insert_db(self, table_name, values):
         if table_name not in self.table:
             print(f"Table {table_name} does not exist!")
@@ -45,8 +63,9 @@ class storager:
             if len(values) != len(self.table[table_name].columns):
                 print(f"Invalid amount of values to insert into {table_name}!")
             else:
-                rows_insert = self.table[table_name].insert(values)
+                rows_insert = self.table[table_name].insert(table_name, values)
                 print(f"{len(rows_insert)} row(s) has been inserted into {table_name}!")
 
 if __name__ == "__main__":
     storage = storager()
+    index = indexColumns()
