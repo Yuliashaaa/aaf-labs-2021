be2396c28c891443b6b4f471c86a0c070cb597aa
Yuliashaaa <ulka.shevchenko@gmail.com>
2021-12-22 16:02:45 +0200

    Update and rename db.py to storage.py

---
+++ shevchenko_fi-92_kozlovska_fi-92/storage.py
@@ -0,0 +1,37 @@
+# DB structure
+
+from sortedcontainers import SortedDict
+
+# Storager
+class storager:
+    def __init__(self):
+        self.table = {}
+
+    def create_db(self, table_name, columns):
+        if columns:
+            self[table_name] = db(table_name, columns)
+            return f"Table {table_name} has been successfully created!"
+        else:
+            return f"Table {table_name} columns are empty!"
+
+if __name__ == "__main__":
+    storage = storager()
+
+# Indexing for columns
+class indexColumns:
+    def __init__(self):
+        self.container = SortedDict()
+
+# Database 
+class db:
+    def __init__(self, table_name, columns):
+        self.name = table_name
+        self.table = {}
+        self.row_id = 0
+        self.columns = {}
+        self.index = {}
+
+        for i, (column_name, index_flag) in enumerate(columns):
+            self.columns[column_name] = i
+            if index_flag == True:
+                self.index[i] = indexColumns()
